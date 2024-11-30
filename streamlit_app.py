0755ab1209b4f57a571259d314af70c44dc504cf
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-11-30 18:42:12 +0530

    Added app title
    Added app title

--- streamlit_app.py
+++ streamlit_app.py
@@ -778,6 +778,12 @@ def pca_plot(data_name, X):
 # Main Function
 def main():
 
+    # Setting the page title
+    # This title will only be visible when running the app locally.
+    # In the deployed app, the title will be displayed as "Title - Streamlit," where "Title" is the one we provide.
+    # If we don't set the title, it will default to "Streamlit"
+    st.set_page_config(page_title="HyperTuneML Platform")
+
     # Giving Title
     st.title("HyperTuneML Platform")
 
